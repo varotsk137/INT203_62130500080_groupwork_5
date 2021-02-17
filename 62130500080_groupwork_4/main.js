2051f991b8efc4bbe69583f361bb9edbe3f763cc
Varot Tirakhuntorn <varot.137@mail.kmutt.ac.th>
2021-02-17 11:11:33 +0700

    [Add] Adding Method to search photo from photo gallery and list them. Adding Method to revert change when cancel. Check if no photo selected and show No Photo.

--- 62130500080_groupwork_4/main.js
+++ 62130500080_groupwork_4/main.js
@@ -1,33 +1,88 @@
 const app = {
     data() {
         return {
-            images: [
-                {image: './images/fullmoon.jpg', name: 'Lone Full moon in the night sky',location:'Shush, Khuzestan Province, Iran', fav: false},
-                {image: './images/galaxy.jpg', name: 'Twinkle Twinkles, That many stars for you',location:'Zolotaya Dolina, Primorsky kray, Russia', fav: false},
-                {image: './images/hillhouse.jpg', name: 'House in the sea of fogs',location:'Unknown', fav: false}
+            images: [{
+                    image: './images/fullmoon.jpg',
+                    name: 'Lone Full moon in the night sky',
+                    location: 'Shush, Khuzestan Province, Iran',
+                    fav: false
+                },
+                {
+                    image: './images/galaxy.jpg',
+                    name: 'Twinkle Twinkles, That many stars for you',
+                    location: 'Zolotaya Dolina, Primorsky kray, Russia',
+                    fav: false
+                },
+                {
+                    image: './images/hillhouse.jpg',
+                    name: 'House in the sea of fogs',
+                    location: 'Unknown',
+                    fav: false
+                }
             ],
             searchModeOn: false,
-            searchValue: ''
+            searchValue: '',
+            searchImages: [{
+                    image: './images/fullmoon.jpg',
+                    name: 'Lone Full moon in the night sky',
+                    location: 'Shush, Khuzestan Province, Iran',
+                    fav: false
+                },
+                {
+                    image: './images/galaxy.jpg',
+                    name: 'Twinkle Twinkles, That many stars for you',
+                    location: 'Zolotaya Dolina, Primorsky kray, Russia',
+                    fav: false
+                },
+                {
+                    image: './images/hillhouse.jpg',
+                    name: 'House in the sea of fogs',
+                    location: 'Unknown',
+                    fav: false
+                }
+            ]
         }
     },
     methods: {
-        toggleHeart(index){
+        toggleHeart(index) {
             this.images[index].fav = !this.images[index].fav
         },
-        toggleSearchMode(){
+        toggleSearchMode() {
+            if (this.searchModeOn) {
+                this.searchImages = this.images
+            }
             this.searchModeOn = !this.searchModeOn;
         },
-        searchPhoto(){
-
+        searchPhoto() {
+            // Check condition if input is null then just return //
+            if (this.searchValue == '') {
+                return
+            }
+            this.searchImages = [];
+            // Push image which include the values in new array //
+            this.images.forEach(image => {
+                console.log(image.name);
+                if (image.name.toLowerCase().includes(this.searchValue.toLowerCase()) 
+                || image.location.toLowerCase().includes(this.searchValue.toLowerCase())) {
+                    this.searchImages.push(image)
+                }
+            })
+            this.searchValue = ''
+        },
+        hasPhoto(){
+            if(this.searchImages.length == 0){
+                return true
+            }
+            return false
         }
     },
     computed: {
         totalPhoto() {
-            return this.images.length
+            return this.searchImages.length
         },
-        // searchPhoto(name) {
-        //
-        // }
+        showGallery() {
+            return this.searchImages
+        }
     }
 }
 
